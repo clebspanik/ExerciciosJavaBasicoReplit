621353ff9aaaf3214f52fd14d4971b9b22be2946
Cleber <104567962+clebspanik@users.noreply.github.com>
2022-06-23 16:46:50 -0300

    Add files via upload

---
+++ Operador de Incremento - java/Main.java
@@ -0,0 +1,11 @@
+//O programa adicionará um número até que o int um seja maior que 1 e até o 99;
+
+//ex. adicone um número até nove milhóes
+
+class Main {
+  public static void main(String[] args) {
+    for (int cc=1; cc<=9000000; cc+=1) {
+      System.out.println(cc);
+    }
+  }
+}
